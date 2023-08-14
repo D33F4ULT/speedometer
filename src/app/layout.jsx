d98e330af343cd5574e97c2c2c0783805b3961cc
D33F4ULT <63258078+D33F4ULT@users.noreply.github.com>
2023-08-14 07:29:12 +0200

    Update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -18,7 +18,7 @@ export default function RootLayout({ children }) {
         />
         <link rel="manifest" href="/manifest.json" />
         {/* <meta name="theme-color" content="#000000" /> */}
-        {/* <link rel="icon" href="/favicon.ico" /> */}
+        <link rel="icon" href="/favicon.ico" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
