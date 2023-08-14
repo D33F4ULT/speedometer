92d81cbf057a49a87b390ccc3cb681caf6631e7e
D33F4ULT <63258078+D33F4ULT@users.noreply.github.com>
2023-08-14 07:06:13 +0200

    pwa

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
-// next.config.js
-const withPWA = require("next-pwa");
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-module.exports = withPWA({
-  pwa: {
-    disable: process.env.NODE_ENV === "development",
-    dest: "public",
-    publicExcludes: ["!icon.png", "!splash.png"], // Add any exclusions
-  },
-  // ... other Next.js config options ...
-});
+module.exports = nextConfig;
