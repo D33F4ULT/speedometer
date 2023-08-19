997a05cc9d191c2ec07015ebc1c928f5814d9020
D33F4ULT <63258078+D33F4ULT@users.noreply.github.com>
2023-08-19 19:43:52 +0200

    Update page.jsx

--- src/app/obd/page.jsx
+++ src/app/obd/page.jsx
@@ -32,7 +32,7 @@ export default function Home() {
   }
 
   // Establish a GATT connection to the device
-  const combinedUUID = "00001101-0000-1000-8000-00805F9B34FB";
+  const combinedUUID = "00001101-0000-1000-8000-00805f9b34fb";
   async function connectToDevice(device) {
     try {
       console.log("Connecting to device...");
